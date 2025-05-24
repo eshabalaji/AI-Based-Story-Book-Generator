16acddaef23f21a4b147a9d43a7b3fa03e9c9488
VJoshiAnkita <vjoshiankita@gmail.com>
2025-05-24 20:46:03 +0530

    updated

--- app.py
+++ app.py
@@ -8,7 +8,7 @@
 '''create a file name as app.py and save the following code and run the below like '''
 #! streamlit run app.py & npx localtunnel --port 8501
 
-'''write the ip address in the page to redirect to'''
+'''write the ip address in the page to redirect to streamlit'''
 #=========================================================================
 
 import streamlit as st
